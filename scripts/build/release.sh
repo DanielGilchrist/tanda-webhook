2f5cf1774120d1107dba13225296f3eb569bcbbe
Daniel Gilchrist <danniieelg@gmail.com>
2023-05-08 01:05:25 +0100

    Make release script more informative

--- scripts/build/release.sh
+++ scripts/build/release.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-crystal build src/tanda_webhook.cr --release --no-debug --progress && mv tanda_webhook bin/tanda-webhook
+# This script assumes you're running it from the root of the project
+# i.e. ./scripts/build-prod.sh
+
+crystal build src/tanda_webhook.cr --release --no-debug --progress --stats \
+  && mv tanda_webhook bin/tanda-webhook \
+  && echo \
+  && printf "\e[32mSuccess:\e[0m compiled release binary to $(pwd)/bin/tanda-webhook\n"
