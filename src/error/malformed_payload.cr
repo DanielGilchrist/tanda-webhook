284ef027c19b502f5571ae483edb1136acaeeaa8
Daniel Gilchrist <daniel@workforce.com>
2023-06-01 18:10:27 +0100

    Fix issue where IO is consumed and can't be read again + handle non JSON responses

--- src/error/malformed_payload.cr
+++ src/error/malformed_payload.cr
@@ -3,21 +3,21 @@ require "./base"
 module Tanda::Webhook
   module Error
     class MalformedPayload < Base
-      def initialize(@body : IO, exception : JSON::SerializableError)
+      MALFORMED_PAYLOAD_TEXT = "Malformed payload:".colorize.bold.red
+
+      def initialize(@body : String, exception)
         super(exception.message)
       end
 
-      def pretty_message
-        pretty_json = try_pretty_parse_body
-        "Malformed payload:\n#{pretty_json}".colorize.red
+      def pretty_message : String
+        pretty_json = try_pretty_parse_body.colorize.light_red
+        "#{MALFORMED_PAYLOAD_TEXT}\n#{pretty_json}"
       end
 
       def try_pretty_parse_body
-        begin
-          JSON.parse(@body).to_pretty_json
-        rescue JSON::ParseException
-          @body.to_s
-        end
+        JSON.parse(@body).to_pretty_json
+      rescue JSON::ParseException
+        @body
       end
     end
   end
--- src/tanda_webhook/types/webhook.cr
+++ src/tanda_webhook/types/webhook.cr
@@ -8,14 +8,24 @@ module Tanda::Webhook
       include JSON::Serializable
 
       def self.from(request : HTTP::Request) : self | Error::Base
-        body = request.body
-        puts body
-        return Error::MissingPayload.new if body.nil?
+        request_body = request.body
+        return Error::MissingPayload.new if request_body.nil?
+
+        # We need to copy the IO to an IO::Memory because if the JSON can't be parsed initially
+        # the IO would be consumed and needs to be rewinded so we can try again
+        body = IO::Memory.new
+        IO.copy(request_body, body)
 
         begin
           from_json(body)
-        rescue error : JSON::SerializableError
-          Error::MalformedPayload.new(body, error)
+        rescue error : JSON::SerializableError | JSON::ParseException
+          begin
+            body.rewind
+            json = body.gets_to_end
+            Error::MalformedPayload.new(json, error)
+          rescue e : JSON::ParseException
+            Error::NonJSONPayload.new(e)
+          end
         end
       end
 
