d9ab3071cdb3dbd45078a32dd83f59df0f92c771
Daniel Gilchrist <daniel@workforce.com>
2023-06-02 13:52:50 +0100

    Fix issue where request counts were being discarded

--- src/tanda_webhook/server.cr
+++ src/tanda_webhook/server.cr
@@ -7,26 +7,11 @@ require "./types/webhook"
 
 module Tanda::Webhook
   class Server
-    HEADERS_STRING        = "Headers:".colorize.yellow
-    BODY_STRING           = "Body:".colorize.yellow
-    REQUEST_COUNTS_STRING = "Request counts:".colorize.yellow
-    SPLITTER              = ("=" * 100).colorize.magenta
-    FILE_TIME_FORMAT      = "%Y-%m-%d-%H-%M-%S"
-    OUTPUT_DIR            = "output"
-    TOTAL                 = "total"
-
-    # {
-    #   "https://some_url.com" => {
-    #     "schedule.published" => 1,
-    #     "schedule.updated" => 2,
-    #   },
-    #   "https://some_other_url.com" => {
-    #     "shift.updated" => 1,
-    #     "schedule.published" => 2,
-    #   },
-    # }
-    alias RequestCounts = Hash(String, TopicCounts)
-    alias TopicCounts = Hash(String, UInt32)
+    HEADERS_STRING   = "Headers:".colorize.yellow
+    BODY_STRING      = "Body:".colorize.yellow
+    SPLITTER         = ("=" * 100).colorize.magenta
+    FILE_TIME_FORMAT = "%Y-%m-%d-%H-%M-%S"
+    OUTPUT_DIR       = "output"
 
     alias KemalContext = HTTP::Server::Context
 
@@ -35,10 +20,7 @@ module Tanda::Webhook
     end
 
     def initialize
-      @requests = {
-        counts:   RequestCounts.new(default_value: TopicCounts.new(default_value: 0)),
-        requests: Array({headers: HTTP::Headers, body: Types::Webhook}).new,
-      }
+      @request_log = RequestLog.new
     end
 
     def run
@@ -48,7 +30,7 @@ module Tanda::Webhook
 
           splitters do
             track_and_log_request(ctx)
-            log_counts
+            @request_log.log_counts
           end
         end
       end
@@ -57,7 +39,7 @@ module Tanda::Webhook
     end
 
     private def write_to_json_file
-      return if request_counts.empty?
+      return if @request_log.empty?
 
       time = Time.local
       formatted_time = Time::Format.new(FILE_TIME_FORMAT).format(time)
@@ -66,7 +48,7 @@ module Tanda::Webhook
       create_output_dir_if_not_exists
 
       puts "Writing output to #{filename}...".colorize.green
-      File.write(filename, content: @requests.to_json)
+      File.write(filename, content: @request_log.to_json)
       puts "Done!".colorize.green
     end
 
@@ -78,32 +60,14 @@ module Tanda::Webhook
       webhook = Types::Webhook.from(ctx.request)
       return webhook.handle! if webhook.is_a?(Error::Base)
 
-      @requests[:requests] << {headers: ctx.request.headers, body: webhook}
-
       url = ctx.request.hostname.to_s
-      topic_counts = request_counts[url]
       topic = webhook.payload.topic
 
-      topic_counts[TOTAL] += 1
-      topic_counts[topic] += 1
+      @request_log.increment_request_counts!(url, topic)
+      @request_log.set_request!(headers: ctx.request.headers, body: webhook)
 
-      pretty_print_obj(HEADERS_STRING, ctx.request.headers.to_h)
-      pretty_print_obj(BODY_STRING, JSON.parse(webhook.to_json))
-    end
-
-    private def request_counts : RequestCounts
-      @requests[:counts]
-    end
-
-    private def log_counts
-      pretty_print_obj(REQUEST_COUNTS_STRING, request_counts)
-    end
-
-    private def pretty_print_obj(header, obj)
-      puts
-      puts header
-      pp obj
-      puts
+      Helpers.pretty_print_obj(HEADERS_STRING, ctx.request.headers.to_h)
+      Helpers.pretty_print_obj(BODY_STRING, JSON.parse(webhook.to_json))
     end
 
     private def splitters(&)
@@ -114,6 +78,68 @@ module Tanda::Webhook
 
     private def print_splitter
       puts SPLITTER
+    end
+
+    private module Helpers
+      extend self
+
+      def pretty_print_obj(header, obj)
+        puts
+        puts header
+        pp obj
+        puts
+      end
+    end
+
+    private class RequestLog
+      include JSON::Serializable
+
+      REQUEST_COUNTS_STRING = "Request counts:".colorize.yellow
+      TOTAL_COUNT_KEY       = "total"
+
+      alias Requests = Array({headers: HTTP::Headers, body: Types::Webhook})
+
+      # {
+      #   "https://some_url.com" => {
+      #     "schedule.published" => 1,
+      #     "schedule.updated" => 2,
+      #   },
+      #   "https://some_other_url.com" => {
+      #     "shift.updated" => 1,
+      #     "schedule.published" => 2,
+      #   },
+      # }
+      alias RequestCounts = Hash(String, TopicCounts)
+      alias TopicCounts = Hash(String, UInt32)
+
+      def initialize
+        @counts = RequestCounts.new do |request_counts, url_or_total|
+          request_counts[url_or_total] = TopicCounts.new do |topic_counts, topic|
+            topic_counts[topic] = 0
+          end
+        end
+        @requests = Requests.new
+      end
+
+      def set_request!(headers : HTTP::Headers, body : Types::Webhook)
+        @requests << {headers: headers, body: body}
+      end
+
+      def increment_request_counts!(url : String, topic : String)
+        @counts[TOTAL_COUNT_KEY][topic] += 1
+        @counts[url][topic] += 1
+      end
+
+      def log_counts
+        Helpers.pretty_print_obj(REQUEST_COUNTS_STRING, @counts)
+      end
+
+      def empty? : Bool
+        @counts.empty?
+      end
+
+      private getter counts : RequestCounts
+      private getter requests : Array({headers: HTTP::Headers, body: Types::Webhook})
     end
   end
 end
